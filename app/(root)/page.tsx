09aeddf19b9e8def6f4939b9b9a5f629a67492c4
Shokhrukh <shokhrukhnuriddinov@gmail.com>
2025-11-03 23:08:05 +0900

    Implement homa page with TradingView widgets

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,53 @@
-import Header from '@/components/shared/header'
+import TradingviewWidget from '@/components/shared/tradingview-widget'
+import {
+	MARKET_DATA_WIDGET_CONFIG,
+	MARKET_OVERVIEW_WIDGET_CONFIG,
+	TOP_STORIES_WIDGET_CONFIG,
+} from '@/constants'
 
 const Home = () => {
+	const scriptUrl =
+		'https://s3.tradingview.com/external-embedding/embed-widget-'
 	return (
 		<div className='flex min-h-screen home-wrapper'>
-			<Header />
+			<section className='grid w-full gap-8 home-section'>
+				<div className='md:col-span-1 xl:col-span-1'>
+					<TradingviewWidget
+						title='Market Overview'
+						scriptUrl={`${scriptUrl}market-overview.js`}
+						config={MARKET_OVERVIEW_WIDGET_CONFIG}
+						className='custom-chart'
+						height={600}
+					/>
+				</div>
+
+				<div className='md:col-span-1 xl:col-span-2'>
+					<TradingviewWidget
+						title='Stock Heatmap'
+						scriptUrl={`${scriptUrl}stock-heatmap.js`}
+						config={MARKET_OVERVIEW_WIDGET_CONFIG}
+						height={600}
+					/>
+				</div>
+			</section>
+
+			<section className='grid w-full gap-8 home-section'>
+				<div className='h-full md:col-span-1 xl:col-span-1'>
+					<TradingviewWidget
+						scriptUrl={`${scriptUrl}timeline.js`}
+						config={TOP_STORIES_WIDGET_CONFIG}
+						height={600}
+					/>
+				</div>
+
+				<div className='h-full md:col-span-1 xl:col-span-2'>
+					<TradingviewWidget
+						scriptUrl={`${scriptUrl}market-quotes.js`}
+						config={MARKET_DATA_WIDGET_CONFIG}
+						height={600}
+					/>
+				</div>
+			</section>
 		</div>
 	)
 }
