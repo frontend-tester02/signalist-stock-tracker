3a294dba6cd153366d5b8fc37a838d021289940c
Shokhrukh <shokhrukhnuriddinov@gmail.com>
2025-11-03 21:44:29 +0900

    Implementlayout and header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/shared/header'
+
+const Home = () => {
+	return (
+		<div className='flex min-h-screen home-wrapper'>
+			<Header />
+		</div>
+	)
+}
+
+export default Home
